df5054bdc281ebe1dfa6a727f10433c85bc9ef21
Roshan Bajgain <you@example.com>
2022-01-04 14:54:01 +0545

    create component file and add class in calculator.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import react from 'react';
+// import calculator from './component/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// class App extends react.Component {
+//   render() {
+//     return <calculator />;
+//   }
+// }
 
-export default App;
+// export default App;
