6f1f6eb00acc81473d32ee5a943016855a409005
Roshan Bajgain <you@example.com>
2022-01-06 09:31:22 +0545

    Add logic in render function

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import calculator from './logic/calculator';
 class App extends react.Component {
  constructor(props){
    super(props);
-   this.state = {total: null, next: null, operate:null};
+   this.state = {total: null, next: null, operation: null};
    this.updateDetails = this.updateDetails.bind(this);
  }
  updateDetails(e) {
    const newObj = calculate(this.state, e.target.textContent)
    this.setState(newObj);
  }
- 
+ render() {
+   const {next, operation, total} = this.state;
+   const op = operation === '%' ? 'mod' : operation;
+   let result = '';
+   if (total) {
+     result = `${total} ${op || ''} ${next || ''}`;
+   } else if (next) {
+     result= `${next} ${op || ''}`;
+   }
+ }
 }
 
 export default App;
