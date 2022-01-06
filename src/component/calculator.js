5aac4354ede941b1a9a7af11ec972651808b3539
Roshan Bajgain <you@example.com>
2022-01-06 13:29:32 +0545

    implement handle click function for each button

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,47 +1,48 @@
 import React from 'react';
 import './calculator.css';
-import PropTypes from 'prop-types';
+import calculate from '../logic/calculator';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
   }
 
   render() {
-    const { updateDetails, value } = this.props;
+    const { next, total } = this.state;
     return (
       <div className="container">
         <div className="screen">
-          {value || 0 }
+          {next || total || 0 }
         </div>
         <div className="buttons">
-          <button onClick={updateDetails} type="button">Ac</button>
-          <button onClick={updateDetails} type="button">+/-</button>
-          <button onClick={updateDetails} type="button">%</button>
-          <button onClick={updateDetails} className="orange" type="button">&divide;</button>
-          <button onClick={updateDetails} type="button">7</button>
-          <button onClick={updateDetails} type="button">8</button>
-          <button onClick={updateDetails} type="button">9</button>
-          <button onClick={updateDetails} className="orange" type="button">&times;</button>
-          <button onClick={updateDetails} type="button">4</button>
-          <button onClick={updateDetails} type="button">5</button>
-          <button onClick={updateDetails} type="button">6</button>
-          <button onClick={updateDetails} className="orange" type="button">&minus;</button>
-          <button onClick={updateDetails} type="button">1</button>
-          <button onClick={updateDetails} type="button">2</button>
-          <button onClick={updateDetails} type="button">3</button>
-          <button onClick={updateDetails} className="orange" type="button">+</button>
-          <button onClick={updateDetails} className="double" type="button">0</button>
-          <button onClick={updateDetails} type="button">.</button>
-          <button onClick={updateDetails} className="orange" type="button">=</button>
+          <button onClick={this.handleClick} type="button">Ac</button>
+          <button onClick={this.handleClick} type="button">+/-</button>
+          <button onClick={this.handleClick} type="button">%</button>
+          <button onClick={this.handleClick} className="orange" type="button">&divide;</button>
+          <button onClick={this.handleClick} type="button">7</button>
+          <button onClick={this.handleClick} type="button">8</button>
+          <button onClick={this.handleClick} type="button">9</button>
+          <button onClick={this.handleClick} className="orange" type="button">&times;</button>
+          <button onClick={this.handleClick} type="button">4</button>
+          <button onClick={this.handleClick} type="button">5</button>
+          <button onClick={this.handleClick} type="button">6</button>
+          <button onClick={this.handleClick} className="orange" type="button">&minus;</button>
+          <button onClick={this.handleClick} type="button">1</button>
+          <button onClick={this.handleClick} type="button">2</button>
+          <button onClick={this.handleClick} type="button">3</button>
+          <button onClick={this.handleClick} className="orange" type="button">+</button>
+          <button onClick={this.handleClick} className="double" type="button">0</button>
+          <button onClick={this.handleClick} type="button">.</button>
+          <button onClick={this.handleClick} className="orange" type="button">=</button>
         </div>
       </div>
     );
   }
 }
-Calculator.PropTypes = {
-  updateDetails: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
-};
 export default Calculator;
