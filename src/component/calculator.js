9e9cf073ad82004f2d4ecbbd340b342fa231398f
Roshan Bajgain <you@example.com>
2022-01-06 18:48:24 +0545

    Add hook funtion to the calculator

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,48 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './calculator.css';
 import calculate from '../logic/calculator';
+// import { useState } from 'react';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, NewState] = useState({});
 
-  handleClick(e) {
-    this.setState((state) => calculate(state, e.target.textContent));
-  }
-
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="container">
-        <div className="screen">
-          {next || total || 0 }
-        </div>
-        <div className="buttons">
-          <button onClick={this.handleClick} type="button">Ac</button>
-          <button onClick={this.handleClick} type="button">+/-</button>
-          <button onClick={this.handleClick} type="button">%</button>
-          <button onClick={this.handleClick} className="orange" type="button">&divide;</button>
-          <button onClick={this.handleClick} type="button">7</button>
-          <button onClick={this.handleClick} type="button">8</button>
-          <button onClick={this.handleClick} type="button">9</button>
-          <button onClick={this.handleClick} className="orange" type="button">&times;</button>
-          <button onClick={this.handleClick} type="button">4</button>
-          <button onClick={this.handleClick} type="button">5</button>
-          <button onClick={this.handleClick} type="button">6</button>
-          <button onClick={this.handleClick} className="orange" type="button">&minus;</button>
-          <button onClick={this.handleClick} type="button">1</button>
-          <button onClick={this.handleClick} type="button">2</button>
-          <button onClick={this.handleClick} type="button">3</button>
-          <button onClick={this.handleClick} className="orange" type="button">+</button>
-          <button onClick={this.handleClick} className="double" type="button">0</button>
-          <button onClick={this.handleClick} type="button">.</button>
-          <button onClick={this.handleClick} className="orange" type="button">=</button>
-        </div>
+  
+  const { next, total } = state;
+  return (
+    <div className="container">
+      <div className="screen">
+        {next || total || 0 }
       </div>
-    );
-  }
-}
+      <div className="buttons">
+        <button onClick={handleClick} type="button">Ac</button>
+        <button onClick={handleClick} type="button">+/-</button>
+        <button onClick={handleClick} type="button">%</button>
+        <button onClick={handleClick} className="orange" type="button">&divide;</button>
+        <button onClick={handleClick} type="button">7</button>
+        <button onClick={handleClick} type="button">8</button>
+        <button onClick={handleClick} type="button">9</button>
+        <button onClick={handleClick} className="orange" type="button">&times;</button>
+        <button onClick={handleClick} type="button">4</button>
+        <button onClick={handleClick} type="button">5</button>
+        <button onClick={handleClick} type="button">6</button>
+        <button onClick={handleClick} className="orange" type="button">&minus;</button>
+        <button onClick={handleClick} type="button">1</button>
+        <button onClick={handleClick} type="button">2</button>
+        <button onClick={handleClick} type="button">3</button>
+        <button onClick={handleClick} className="orange" type="button">+</button>
+        <button onClick={handleClick} className="double" type="button">0</button>
+        <button onClick={handleClick} type="button">.</button>
+        <button onClick={handleClick} className="orange" type="button">=</button>
+      </div>
+    </div>
+  );
+};
 export default Calculator;
