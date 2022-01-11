4b696b7ad09ae4e4aab6f220d1a3cd5da178b44b
Roshan Bajgain <you@example.com>
2022-01-11 18:29:16 +0545

    Add switch and router path to the 3 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import react from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Calculator from './component/calculator';
+import Header from './component/Header';
 
 class App extends react.Component {
   render() {
     return (
       <div>
+        <Header />
+        <switch>
+          <Route path = '/'>
+            <Home/>
+          </Route>
+          <Route path = '/calculator'>
+            <Calculator/>
+          </Route>
+          <Route path = '/quote'>
+            <Quote/>
+          </Route>
+        </switch>
         <Calculator />
       </div>
     );
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,14 +1,14 @@
 const Header = () => {
-    <header>
-        <h1>Math Magicians</h1>
-        <nav>
-            <ul>
-                <link to ="/">Home</link>
-                <link to ="/calculator">Calculator</link>
-                <link to ="/quote">quote</link>
-            </ul>
-        </nav>
-    </header>
+  <header>
+    <h1>Math Magicians</h1>
+    <nav>
+      <ul>
+        <link to="/">Home</link>
+        <link to="/calculator">Calculator</link>
+        <link to="/quote">Quote</link>
+      </ul>
+    </nav>
+  </header>;
 };
 
 export default Header;
