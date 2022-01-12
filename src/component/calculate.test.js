65cb98e67d124206d91315de3d5b677d477aaf64
Roshan Bajgain <you@example.com>
2022-01-12 20:59:19 +0545

    Add testing for component

--- src/component/calculate.test.js
+++ src/component/calculate.test.js
@@ -1,12 +1,11 @@
 // Test for component/calculate.js
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
 import Calculator from './calculator';
 
-it('renders without crashing', () => {
-  render(<Calculator />);
-  expect((<Calculator />)).toBeInTheDocument();
+it('renders correctly', () => {
+  const tree = renderer.create(<Calculator />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
 
 
